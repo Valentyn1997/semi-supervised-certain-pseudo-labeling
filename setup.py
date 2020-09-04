a99d4a48c8f4a5c71ec1c43790c7b2885349d5eb
Valentyn Melnychuk <v.melnychuk@campus.lmu.de>
2020-09-04 14:43:39 +0200

    Add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applying uncertanty estimation to semi-supervised image classification methods',
+    author='Valentyn Melnychuk',
+    license='MIT',
+)
