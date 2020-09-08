d67a435b0fbe4b86e3a666426f8ed808b2c64dab
Valentyn Melnychuk <v.melnychuk@campus.lmu.de>
2020-09-08 22:14:03 +0200

    Basic implementation of FixMatch

---
+++ src/models/fix_match.py
@@ -0,0 +1,110 @@
+from pytorch_lightning import LightningModule
+from omegaconf import DictConfig
+import torch
+import torch.nn.functional as F
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+import numpy as np
+
+from src.utils import simple_accuracy
+from src.models.backbones import WideResNet
+from src.data.ssl_datasets import SLLDatasetsCollection, FixMatchCompositeTrainDataset
+
+
+class FixMatch(LightningModule):
+    def __init__(self, args: DictConfig, datasets_collection: SLLDatasetsCollection):
+        super().__init__()
+        self.lr = None  # Placeholder for auto_lr_find
+        self.datasets_collection = datasets_collection
+        self.model = WideResNet(depth=28, widen_factor=2, drop_rate=0.0, num_classes=len(datasets_collection.classes))
+        self.best_model = self.model  # Placeholder for checkpointing
+        self.hparams = args  # Will be logged to mlflow
+
+    def prepare_data(self):
+        self.train_dataset = FixMatchCompositeTrainDataset(self.datasets_collection.train_l_dataset,
+                                                           self.datasets_collection.train_ul_dataset,
+                                                           self.hparams.model.mu)
+        self.val_dataset = self.datasets_collection.val_dataset
+        self.test_dataset = self.datasets_collection.test_dataset
+
+    def configure_optimizers(self):
+        if self.lr is not None:  # After auto_lr_find
+            self.hparams.optimizer.lr = self.lr
+        return SGD(self.model.parameters(), lr=self.hparams.optimizer.lr, momentum=self.hparams.optimizer.momentum,
+                   nesterov=self.hparams.optimizer.nesterov)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.train_dataset, shuffle=True, batch_size=self.hparams.data.batch_size.train, num_workers=2)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.val_dataset, shuffle=False, batch_size=self.hparams.data.batch_size.val)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.test_dataset, shuffle=False, batch_size=self.hparams.data.batch_size.test)
+
+    def forward(self, batch):
+        return self.model(batch)
+
+    def training_step(self, composite_batch, batch_idx):
+        l_targets = composite_batch[0][0][1]
+        l_images = composite_batch[0][0][0]
+
+        ul_targets = torch.cat([item[1] for item in composite_batch[1]])  # For later checks
+        uw_images = torch.cat([item[0][0] for item in composite_batch[1]])
+        us_images = torch.cat([item[0][1] for item in composite_batch[1]])
+
+        # Supervised loss
+        logits_l = self(l_images)
+        loss_l = F.cross_entropy(logits_l, l_targets, reduction='mean')
+
+        # Unsupervised loss
+        logits_us = self(us_images)
+        with torch.no_grad():
+            logits_uw = self(uw_images)
+            pseudo_label = torch.softmax(logits_uw.detach(), dim=-1)
+        max_probs, targets_u = torch.max(pseudo_label, dim=-1)
+        mask = max_probs.ge(self.hparams.model.threshold).float()
+        loss_ul = (F.cross_entropy(logits_us, targets_u, reduction='none') * mask).mean()
+
+        # Train loss / labelled accuracy
+        loss = loss_l + self.hparams.model.lambda_u * loss_ul
+
+        return {'loss': loss, 'loss_l': loss_l, 'loss_ul': loss_ul}
+
+    def validation_step(self, batch, batch_idx):
+        results = {}
+        images, targets = batch
+        logits = self(images)
+        loss = F.cross_entropy(logits, targets, reduction='mean')
+        results['loss'] = loss.detach()
+        results['preds'] = logits.detach()
+        results['labels'] = targets.detach()
+        return results
+
+    test_step = validation_step
+
+    def training_epoch_end(self, outputs):
+        loss = np.array([x['loss'].mean().item() for x in outputs]).mean()
+        loss_l = np.array([x['loss_l'].mean().item() for x in outputs]).mean()
+        loss_ul = np.array([x['loss_ul'].mean().item() for x in outputs]).mean()
+        mlflow_metrics = {'train_loss': loss, 'train_loss_l': loss_l, 'train_loss_ul': loss_ul}
+        return {'loss': loss, 'log': mlflow_metrics}
+
+    def validation_epoch_end(self, outputs):
+        loss_val_mean = torch.tensor(np.array([x['loss'].cpu().numpy() for x in outputs]).mean())
+        mlflow_metrics = {'val_loss': loss_val_mean, **self.calculate_metrics(outputs, prefix='val')}
+        return {'loss': loss_val_mean, 'log': mlflow_metrics}
+
+    def test_epoch_end(self, outputs):
+        loss_test_mean = torch.tensor(np.array([x['loss'].cpu().numpy() for x in outputs]).mean())
+        mlflow_metrics = {'test_loss': loss_test_mean, **self.calculate_metrics(outputs, prefix='test')}
+        return {'loss': loss_test_mean, 'log': mlflow_metrics}
+
+    @staticmethod
+    def calculate_metrics(outputs, prefix):
+        preds = np.array([prob for x in outputs for prob in x['preds'].cpu().numpy()])
+        preds = np.argmax(preds, axis=1)
+        labels = np.array([label for x in outputs for label in x['labels'].cpu().numpy()])
+        return {
+            prefix + '_acc': simple_accuracy(preds, labels)
+        }
---
+++ src/utils.py
@@ -0,0 +1,36 @@
+import logging
+from scipy.stats import pearsonr, spearmanr
+import torch
+import random
+import numpy as np
+from omegaconf import DictConfig
+from copy import deepcopy
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+logger = logging.getLogger(__name__)
+
+
+def set_seed(args: DictConfig):
+    random.seed(args.exp.seed)
+    np.random.seed(args.exp.seed)
+    torch.manual_seed(args.exp.seed)
+    if torch.cuda.device_count() > 0:
+        torch.cuda.manual_seed_all(args.exp.seed)
+
+
+class CustomModelCheckpoint(ModelCheckpoint):
+    def __init__(self, model, **kwargs):
+        super().__init__(**kwargs)
+        self.model = model
+
+    # modify saving
+    def _save_model(self, filepath, trainer, pl_module):
+        self.model.best_model = deepcopy(self.model.model)
+        if self.model.hparams.exp.logging:
+            self.model.trainer.logger.log_metrics({'best_epoch': self.model.trainer.current_epoch + 1},
+                                                  step=self.model.trainer.global_step)
+
+
+def simple_accuracy(preds, labels):
+    return (preds == labels).mean()
