Change train_loader size, increase max steps to 2**20, add 16-bit precision, add cosine lr-sceduler with warmup, increase cpu usage in slurm script, fix normalisation for cifar-10/cifar-100, add weak augmentation before strong, fix issue with checkpointing, fix forward pass and batch normalisation issue (simultaneous update), fix ema-optimizer, remove weight decay for bias and bn in SGD

src/data/ssl_datasets.py
@@ -41,15 +41,16 @@ class SLLDatasetsCollection:
             self.test_dataset = self.Dataset(root=self.dataset_path, split='test', download=True)
 
         self.train_dataset, self.val_dataset = self.split_train_val(self.train_dataset, val_ratio, random_state)
-        self.train_l_dataset, self.train_ul_dataset = self.remove_labels_and_split(self.train_dataset, n_labelled, shuffle=False)
+        self.train_l_dataset, self.train_ul_dataset = self.remove_labels_and_split(self.train_dataset, n_labelled, shuffle=True)
         self.classes = self.test_dataset.classes
 
         # Setting transforms
         self.train_dataset.transform = test_transform
         self.train_l_dataset.transform = train_l_transform
         self.train_ul_dataset.transform = train_ul_transform
-        self.val_dataset.transform = test_transform
         self.test_dataset.transform = test_transform
+        if val_ratio > 0.0:
+            self.val_dataset.transform = test_transform
 
     @staticmethod
     def split_train_val(dataset: VisionDataset, val_ratio: float, random_state: int):
@@ -62,16 +63,19 @@ class SLLDatasetsCollection:
         labels = np.array(getattr(dataset, labels_attr))
 
         train_set = deepcopy(dataset)
-        val_set = deepcopy(dataset)
 
-        splitter = StratifiedShuffleSplit(n_splits=1, test_size=val_ratio, random_state=random_state)
-        for train_index, val_index in splitter.split(dataset.data, labels):
-            train_set.data = dataset.data[train_index]
-            setattr(train_set, labels_attr, labels[train_index])
-            val_set.data = dataset.data[val_index]
-            setattr(val_set, labels_attr, labels[val_index])
+        if val_ratio > 0.0:
+            val_set = deepcopy(dataset)
+            splitter = StratifiedShuffleSplit(n_splits=1, test_size=val_ratio, random_state=random_state)
+            for train_index, val_index in splitter.split(dataset.data, labels):
+                train_set.data = dataset.data[train_index]
+                setattr(train_set, labels_attr, labels[train_index])
+                val_set.data = dataset.data[val_index]
+                setattr(val_set, labels_attr, labels[val_index])
 
-        return train_set, val_set
+            return train_set, val_set
+        else:
+            return train_set, None
 
     @staticmethod
     def remove_labels_and_split(dataset: VisionDataset, n_labels_to_leave: int, shuffle=False):
@@ -121,14 +125,17 @@ class SLLDatasetsCollection:
 
 class FixMatchCompositeTrainDataset(VisionDataset):
     """Wrapper, to generate composite item (labelled, unlabelled)"""
-    def __init__(self, l_dataset: VisionDataset, ul_dataset: VisionDataset, mu: int, len_mode='min'):
+    def __init__(self, l_dataset: VisionDataset, ul_dataset: VisionDataset, mu: int, size='max'):
         self.l_dataset = l_dataset
         self.ul_dataset = ul_dataset
         self.mu = mu
 
         self.l_indexes = []
         self.ul_indexes = []
-        self.len = eval(len_mode)(len(self.l_dataset), len(self.ul_dataset) // self.mu)
+        if isinstance(size, int):
+            self.len = size
+        else:
+            self.len = eval(size)(len(self.l_dataset), len(self.ul_dataset) // self.mu)
         self.construct_indices()
 
     def __getitem__(self, i):

src/models/backbones.py
@@ -12,15 +12,15 @@ logger = logging.getLogger(__name__)
 # Credits to https://github.com/kekmodel/FixMatch-pytorch/blob/master/models/wideresnet.py
 
 
-class PSBatchNorm2d(nn.BatchNorm2d):
-    """How Does BN Increase Collapsed Neural Network Filters? (https://arxiv.org/abs/2001.11216)"""
-
-    def __init__(self, num_features, alpha=0.1, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True):
-        super().__init__(num_features, eps, momentum, affine, track_running_stats)
-        self.alpha = alpha
-
-    def forward(self, x):
-        return super().forward(x) + self.alpha
+# class PSBatchNorm2d(nn.BatchNorm2d):
+#     """How Does BN Increase Collapsed Neural Network Filters? (https://arxiv.org/abs/2001.11216)"""
+#
+#     def __init__(self, num_features, alpha=0.1, eps=1e-05, momentum=0.001, affine=True, track_running_stats=True):
+#         super().__init__(num_features, eps, momentum, affine, track_running_stats)
+#         self.alpha = alpha
+#
+#     def forward(self, x):
+#         return super().forward(x) + self.alpha
 
 
 class BasicBlock(nn.Module):
@@ -35,9 +35,9 @@ class BasicBlock(nn.Module):
                                           bias=False)
             self.conv2 = WeightDropConv2d(weight_dropout, out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
 
-        self.bn1 = PSBatchNorm2d(in_planes, momentum=0.001)
+        self.bn1 = nn.BatchNorm2d(in_planes, momentum=0.001)
         self.relu1 = nn.LeakyReLU(negative_slope=0.1, inplace=True)
-        self.bn2 = PSBatchNorm2d(out_planes, momentum=0.001)
+        self.bn2 = nn.BatchNorm2d(out_planes, momentum=0.001)
         self.relu2 = nn.LeakyReLU(negative_slope=0.1, inplace=True)
         self.drop_rate = drop_rate
         self.equalInOut = (in_planes == out_planes)
@@ -96,7 +96,7 @@ class WideResNet(nn.Module):
         # 3rd block
         self.block3 = NetworkBlock(n, channels[2], channels[3], block, 2, drop_rate, weight_dropout, dropout_method)
         # global average pooling and classifier
-        self.bn1 = PSBatchNorm2d(channels[3], momentum=0.001)
+        self.bn1 = nn.BatchNorm2d(channels[3], momentum=0.001)
         self.relu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
         if weight_dropout == 0.0:
             self.fc = nn.Linear(channels[3], num_classes)
@@ -108,7 +108,7 @@ class WideResNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, WeightDropConv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
-            elif isinstance(m, PSBatchNorm2d):
+            elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1.0)
                 nn.init.constant_(m.bias, 0.0)
             elif isinstance(m, nn.Linear) or isinstance(m, WeightDropLinear):
@@ -126,3 +126,13 @@ class WideResNet(nn.Module):
         if self.after_bn_drop_rate > 0.0:
             out = F.dropout(out, p=self.after_bn_drop_rate, training=self.training)
         return self.fc(out)
+
+    def disable_batch_norm_update(self):
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.eval()
+
+    def enable_batch_norm_update(self):
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.train()

src/models/utils.py
@@ -1,44 +1,47 @@
 from pytorch_lightning.core.step_result import TrainResult
 import pandas as pd
 import torch
-from baal.bayesian import MCDropoutConnectModule
+import math
 import numpy as np
 from src.utils import simple_accuracy
+from copy import deepcopy
+from torch.optim.lr_scheduler import LambdaLR
 
 
 class WeightEMA(object):
-    def __init__(self, model, ema_model, alpha=0.999, weight_decay=None, lr=None):
+    def __init__(self, model, ema_model, alpha=0.999):
         self.model = model
         self.ema_model = ema_model
+        self.ema_model.eval()
+
         self.alpha = alpha
-        self.weight_decay = weight_decay
-        self.lr = lr
-
-        params = self.model.parent_module.state_dict() if isinstance(self.model, MCDropoutConnectModule) \
-            else self.model.state_dict()
-        ema_params = self.ema_model.state_dict()
-
-        for key in params.keys():
-            if key in ema_params:
-                ema_params[key].data.copy_(params[key].data)
+        self.ema_has_module = hasattr(self.ema_model, 'module')
+        # Fix EMA. https://github.com/valencebond/FixMatch_pytorch thank you!
+        self.param_keys = [k for k, _ in self.ema_model.named_parameters()]
+        self.buffer_keys = [k for k, _ in self.ema_model.named_buffers()]
+        for p in self.ema_model.parameters():
+            p.requires_grad_(False)
 
     def step(self):
-        one_minus_alpha = 1.0 - self.alpha
+        needs_module = hasattr(self.model, 'module') and not self.ema_has_module
+        with torch.no_grad():
+            msd = self.model.state_dict()
+            esd = self.ema_model.state_dict()
+            for k in self.param_keys:
+                if needs_module:
+                    j = 'module.' + k
+                else:
+                    j = k
+                model_v = msd[j].detach()
+                ema_v = esd[k]
+                esd[k].copy_(ema_v * self.alpha + (1. - self.alpha) * model_v)
 
-        params = self.model.parent_module.state_dict() if isinstance(self.model, MCDropoutConnectModule) \
-            else self.model.state_dict()
-        ema_params = self.ema_model.state_dict()
-
-        for key in params.keys():
-            if key in ema_params:
-                if len(params[key].shape) > 0:
-                    ema_params[key].mul_(self.alpha)
-                    ema_params[key].add_(params[key] * one_minus_alpha)
-
-        if self.weight_decay is not None and self.lr is not None:
-            for param in params.values():
-                if len(param.shape) > 0:
-                    param.mul_(1 - self.weight_decay * self.lr)
+            for k in self.buffer_keys:
+                if needs_module:
+                    j = 'module.' + k
+                else:
+                    j = k
+                esd[k].copy_(msd[j])
 
 
 class UnlabelledStatisticsLogger:
@@ -90,3 +93,18 @@ class UnlabelledStatisticsLogger:
             csv_path = f'{self.artifacts_path}/epochs_{epochs_range[0]:05d}_{epochs_range[1]:05d}.csv'
             self.logging_df.to_csv(csv_path, index=False)
             self.logging_df = pd.DataFrame()
+
+
+def get_cosine_schedule_with_warmup(optimizer,
+                                    num_warmup_steps,
+                                    num_training_steps,
+                                    num_cycles=7./16.,
+                                    last_epoch=-1):
+    def _lr_lambda(current_step):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        no_progress = float(current_step - num_warmup_steps) / \
+            float(max(1, num_training_steps - num_warmup_steps))
+        return max(0., math.cos(math.pi * num_cycles * no_progress))
+
+    return LambdaLR(optimizer, _lr_lambda, last_epoch)

src/utils.py
@@ -27,11 +27,12 @@ class CustomModelCheckpoint(ModelCheckpoint):
     # modify saving
     def _save_model(self, filepath, trainer, pl_module):
         self.model.best_model = deepcopy(self.model.ema_model)
+        self.model.best_model.eval()
         if self.model.hparams.exp.logging:
-            self.model.trainer.logger.log_metrics({'best_epoch': self.model.trainer.current_epoch + 1},
+            self.model.trainer.logger.log_metrics({'best_epoch': self.model.trainer.current_epoch},
                                                   step=self.model.trainer.global_step)
 
 
-def simple_accuracy(logits, labels):
-    preds = np.argmax(logits, axis=1)
-    return (preds == labels).mean()
+def simple_accuracy(logits: torch.Tensor, labels: torch.Tensor):
+    _, preds = torch.max(logits, dim=-1)
+    return (preds == labels).float().mean()